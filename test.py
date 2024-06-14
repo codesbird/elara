85349c73e2c6fb94a9700b8720c568f52d6fb60d
code birds <89650675+codesbird@users.noreply.github.com.>
2024-06-14 22:02:09 +0530

    add new videos

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
 import os
 
-for i,item in enumerate(os.listdir("image")):
-    os.rename(os.path.join('image',item),os.path.join('image',f"image_{i+1}.png"))
+
+#this is for image
+images = os.listdir("img")
+i = len(os.listdir("image"))
+
+for item in images:
+    i+=1
+    os.rename(os.path.join('img',item),os.path.join('image',f"image_{i}.png"))
+
+
+#this is for videos 
+videos = os.listdir("vid")
+i = len(os.listdir("video"))
+
+for item in videos:
+    i+=1
+    os.rename(os.path.join('vid',item),os.path.join('video',f"video_{i}.mp4"))
+
 
